30afd5f323f7a39963488d01818d04c3f62201a4
Ivan_Fediaev <ivan_fediaev@epam.com>
2021-02-02 16:18:07 +0300

    Fix cors policy

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const usersService = require('./services/users.service');
 const todosService = require('./services/todos.service');
@@ -6,6 +7,12 @@ const todosService = require('./services/todos.service');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));s
 
 app.get('/todos', (req, res) => {
   res.send(todosService.getTodos())
@@ -18,12 +25,7 @@ app.get('/todos/:userId', (req, res) => {
 
 app.get('/users', (req, res) => {
   res.send(usersService.getUsers());
-})
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('pages/index'));
+});
 
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
